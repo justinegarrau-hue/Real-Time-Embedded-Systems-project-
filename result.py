32017c3c58eac3888aa5cdd542c2e9d02b16ba5a
justinegarrau-hue <justine.garrau@gmail.com>
2026-04-09 19:27:00 +0200

    Modify execution time analysis for 10,000 iterations
    Updated the script to run 10,000 iterations and output results to a new file. Adjusted print statements for clarity and fixed a typo in the y-label.

--- result.py
+++ result.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 time = []
-open("output.txt","w").close()
-for i in range(100):
-	os.system("/usr/bin/time -f '%e' ./multip 2>>output.txt")
+open("output10000.txt","w").close()
+for i in range(10000):
+	os.system("/usr/bin/time -f '%e' ./multip 2>>output10000.txt")
 
 with open("output.txt", "r", encoding="utf-8") as file:
 	for line in file:
@@ -14,11 +14,14 @@ with open("output.txt", "r", encoding="utf-8") as file:
 
 Q1,Q2,Q3 = np.quantile(time, [0.25, 0.5, 0.75])
 print(f"min: {np.min(time)}s")
-print(f"Q1 (25%) : {Q1}s")
-print(f"Médiane (50%) : {Q2}s")
-print(f"Q3 (75%) : {Q3}s")
+print(f"Q1: {Q1}s")
+print(f"Q2: {Q2}s")
+print(f"Q3: {Q3}s")
 print(f"max: {np.max(time)}s")
-print(f"WCET:{np.max(time)*1.2}s")
+print(f"WCET: {np.max(time)*1.2}s")
 
 plt.hist(time)
+plt.xlabel("execution time (s)")
+plt.ylabl("N")
+plt.title("execution times for 10 000 multiplication command")
 plt.show()
